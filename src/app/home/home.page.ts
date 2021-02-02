da8253b786e20e49188cc1e27961fb176f089402
Paolo Zuccaro <zuccaro.paolo@gmail.com>
2021-02-02 13:18:19 +0100

    - Corretto bug che non visualizzava correttamente appuntamenti se data di oggi aveva giorno ad una cifra e appuntamento a due cifre

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -81,11 +81,11 @@ export class HomePage {
       // Ottengo gli appuntamenti
       this.homeService.getAppointments().then(res => {
         this.appointments = [];
-        const now = format(new Date(), 'M/d/y HH:mm', { locale: it });
+        const now = format(new Date(), 'M/dd/y HH:mm', { locale: it });
 
         // Tengo solo gli appuntamenti nel futuro (considerando al minuto)
         for (let i = 0; i < res.length; i++) {
-          const dateFormatted = format(new Date(res[i].day + ' ' + res[i].startTime), 'M/d/y HH:mm', { locale: it });
+          const dateFormatted = format(new Date(res[i].day + ' ' + res[i].startTime), 'M/dd/y HH:mm', { locale: it });
           if (dateFormatted > now) {
             this.appointments.push(res[i]);
           }
